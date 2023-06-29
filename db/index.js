1c8eb62cf374a76fa18d2f52cc0d7bf5f42593ea
akumakumu <akumakumu@gmail.com>
2023-06-29 22:53:48 +0700

    base

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const { urlDB } = require('../config');
+
+mongoose.connect(urlDB, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+module.exports = db;
